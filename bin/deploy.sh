f553a0b62e9dcc42acf806710bc2a21333dfa018
steinwei <weishijiestein@outlook.com>
2020-06-25 19:47:58 +0800

    Publishing to gh-pages (publish.sh)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,22 +4,22 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-rm -rf .git/worktrees/public/
+echo "Deleting old docsation"
+rm -rf docs
+mkdir docs
+rm -rf .git/worktrees/docs/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out gh-pages branch into docs"
+git worktree add -B gh-pages docs origin/gh-pages
 
 echo "Removing existing files"
-rm -rf public/*
+rm -rf docs/*
 
 echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd docs && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Push to origin"
 git push origin gh-pages
